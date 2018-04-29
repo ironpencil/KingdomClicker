b770abd7e36fc72152d0483b300cdbd016f280d5
Jim South <james.a.south@gmail.com>
2018-04-29 14:30:39 -0400

    User has one kingdom

--- test/models/kingdom_test.rb
+++ test/models/kingdom_test.rb
@@ -3,9 +3,7 @@ require 'test_helper'
 class KingdomTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
-    # This code is not idiomatically correct.
-    @kingdom = Kingdom.new(name: "Lorem ipsum", description: "Dolor sit amet.",
-                            gold: 0, user_id: @user.id)
+    @kingdom = @user.build_kingdom(name: "Lorem ipsum", description: "Dolor sit amet.", gold: 0)
   end
 
   test "should be valid" do
@@ -22,7 +20,7 @@ class KingdomTest < ActiveSupport::TestCase
     assert_not @kingdom.valid?
   end
 
-  test "name should at most 16 characters" do
+  test "name should be at most 16 characters" do
     @kingdom.name = "a" * 17
     assert_not @kingdom.valid?
   end
