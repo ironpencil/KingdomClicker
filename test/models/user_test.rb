a3f81dc9bde3e41a157d5a59832e7c17b02c31ae
Jim South <james.a.south@gmail.com>
2018-04-29 16:47:52 -0400

    Destroy kingdom when user is destroyed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -76,4 +76,12 @@ class UserTest < ActiveSupport::TestCase
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
   end
+
+  test "associated kingdom should be destroyed" do
+    @user.save
+    @user.create_kingdom!(name: "Lorem Ipsum", description: "Dolor sit amet", gold: 0)
+    assert_difference 'Kingdom.count', -1 do
+      @user.destroy
+    end
+  end
 end
